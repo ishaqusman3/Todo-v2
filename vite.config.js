ad2429f0f098d9cc23617f0bb7def254503ed977
Ishaq Usman <ishaquusmanu3@gmail.com>
2024-07-28 11:54:56 +0100

    vite-config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import dotenv from 'dotenv';
+
+// // Load .env file if it exists
+// dotenv.config();
+
+// export default defineConfig({
+//   plugins: [react()],
+//   define: {
+//     'process.env': process.env,
+//   },
+// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
 
-// Load .env file if it exists
-dotenv.config();
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': process.env,
-  },
 });
